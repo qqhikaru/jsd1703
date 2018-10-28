701d3481127f84a874f9273b25e074dcea1d6b24
zhangzhiqiang <qqhikaru@163.com>
2018-10-28 09:05:46 +0800

    2222

--- src/day01/Test.java
+++ src/day01/Test.java
@@ -4,17 +4,6 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		int i=222;
-
-
-		System.out.println("我是中国人");
-		System.out.println("我不是中国人");
-			int j=11;	
-			int ij=12;
-
-		
-			
-
 	}
 
 }
