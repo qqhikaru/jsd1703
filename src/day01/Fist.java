1547a49e8efae1688094e541ae2fe4ee74f3fc72
zhangzhiqiang <qqhikaru@163.com>
2018-11-03 12:20:37 +0800

    zhangsan

--- src/day01/Fist.java
+++ src/day01/Fist.java
@@ -3,6 +3,8 @@ package day01;
 public class Fist {
 	public static void main(String[] args) {
 		System.out.println("zhangsan1");
+		System.out.println("zhangsan1");
+		System.out.println("zhangsan1");
 
 	}
 	
