c7bb5fe13a7e20b0e29bc6935aa3a9753dd2242c
zhangzhiqiang <qqhikaru@163.com>
2018-11-03 12:11:55 +0800

    zhangsan1

--- src/day01/Fist.java
+++ src/day01/Fist.java
@@ -2,24 +2,9 @@ package day01;
 
 public class Fist {
 	public static void main(String[] args) {
-		
+		System.out.println("zhangsan1");
 
 	}
 	
-	public void lisi(){
 
-
-		System.out.println("zhangsan111方法.");
-
-		System.out.println("zhangsan方法2222.");
-
-		System.out.println("增加了一个方法");
-		System.out.println("lisi方法");
-
-
-	}
-	public void zhangsan(){
-		
-
-	}
 }
